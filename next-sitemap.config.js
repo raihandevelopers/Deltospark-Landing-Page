9bdba8ca6834b40ff4493dbc54754efd4893a070
raihandevelopers <raihankhan.indore@gmail.com>
2024-09-11 20:04:54 +0530

    Sitemap and Robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://deltospark.com', // Replace with your website URL
+    generateRobotsTxt: true, // Optional: Generate a robots.txt file
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
